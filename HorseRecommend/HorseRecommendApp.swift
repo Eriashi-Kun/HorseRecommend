893de2c79286d9bdc52252465bd58add6030e36c
eriashi <j.zargo@icloud.com>
2026-04-26 10:45:46 +0900

    Initial Commit

---
+++ HorseRecommend/HorseRecommendApp.swift
@@ -0,0 +1,17 @@
+//
+//  HorseRecommendApp.swift
+//  HorseRecommend
+//
+//  Created by Shutaro Makino on 2026/04/26.
+//
+
+import SwiftUI
+
+@main
+struct HorseRecommendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
