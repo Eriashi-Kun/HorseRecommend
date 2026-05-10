f206de5a27fe19d020e86318e7126b63b1148f9e
eriashi <j.zargo@icloud.com>
2026-05-10 16:16:52 +0900

    Set production backend URL

--- HorseRecommend/Config.swift
+++ HorseRecommend/Config.swift
@@ -4,6 +4,6 @@ enum Config {
     #if DEBUG
     static let backendURL = "http://localhost:8000"
     #else
-    static let backendURL = "https://your-app.onrender.com"  // Renderデプロイ後に更新
+    static let backendURL = "https://horse-recommend.onrender.com"
     #endif
 }
